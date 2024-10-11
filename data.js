8ef65931f12e292ae53da3ee03fb9860ab72e02f
Kuma3mccm <55430208+Kuma3mccm@users.noreply.github.com>
2024-10-12 02:42:18 +0900

    Update data.js

--- data.js
+++ data.js
@@ -25,18 +25,6 @@ class SharedData {
             },
 
             {
-                "name": "hat",
-                "parent": "head",
-                "pivot": [ 0.0, 24.0, 0.0 ]
-            },
-
-            {
-                "name": "helmet",
-                "parent": "head",
-                "pivot": [ 0.0, 24.0, 0.0 ]
-            },
-
-            {
                 "name": "cape",
                 "pivot": [ 0.0, 24, 3.0 ],
                 "parent": "body"
@@ -106,54 +94,7 @@ class SharedData {
                 "name": "jacket",
                 "parent": "body",
                 "pivot": [ 0.0, 24.0, 0.0 ]
-            },
-
-            {
-                "name": "flip",
-                "parent": "head",
-                "pivot": [ 3.7, 5.05, 4.75 ],
-                "rotation": [ 0.0, -90.0, 15.0]
-            },
-
-            {
-                "name": "neck",
-                "parent": "flip",
-                "pivot": [ 3.7, 5.05, 4.75 ]
-            },
-
-            {
-                "name": "tail",
-                "parent": "head",
-                "pivot": [ 0.0, 19.4, 4.5 ]
-            },
-
-            {
-                "name": "frontlegr",
-                "parent": "body",
-                "pivot": [ 0.0, 6.625, 1.0 ],
-                "rotation": [ 0.0, 22.5, 90.0]
-            },
-
-            {
-                "name": "frontlegl",
-                "parent": "frontlegr",
-                "pivot": [ 0.0, 6.625, 2.35 ],
-                "rotation": [ -25.5778, -14.2906, 4.6066]
-            },
-
-            {
-                "name": "backlegl",
-                "parent": "frontlegl",
-                "pivot": [ 0.0, 6.625, 1.0 ],
-                "rotation": [ -25.5778, -14.2906, 4.6066]
-            },
-
-            {
-                "name": "backlegr",
-                "parent": "backlegl",
-                "pivot": [ 0.0, 6.625, 1.0 ],
-                "rotation": [ 21.2449, 22.3749, 0.7409]
-            },
+            }
         ];
     }
 
